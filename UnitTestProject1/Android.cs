ea9477bc613ddee49a294c6de3b6f287b713bf7d
admin <admin@DESKTOP-HG3J3T8>
2020-07-28 00:13:10 -0400

    adding ios

--- UnitTestProject1/Android.cs
+++ UnitTestProject1/Android.cs
@@ -16,19 +16,23 @@ namespace UnitTestProject1
 	{
 		private AppiumLocalService _appiumLocalService;
 
-		public AndroidDriver<IWebElement> CreateAndroidDriver(string deviceIpPort, string deviceName = "")
+		public AndroidDriver<IWebElement> CreateDriver(string deviceIpPort, string deviceName = "")
 		{
+			Environment.SetEnvironmentVariable(AppiumServiceConstants.NodeBinaryPath, @"C:\Program Files\nodejs\node.exe");
+
 			if (string.IsNullOrWhiteSpace(deviceName))
 				deviceName = deviceIpPort;
 
 			// start appium service
 			var builder = new AppiumServiceBuilder();
-			_appiumLocalService = builder.UsingAnyFreePort().Build();
+			//_appiumLocalService = builder.UsingAnyFreePort().Build();
+			_appiumLocalService = builder.UsingPort(4724).Build();
 			_appiumLocalService.Start();
 			_appiumLocalService.OutputDataReceived += (sender, args) =>
 													{
 														Console.WriteLine(args.Data);
 													};
+
 			// create appium driver capabilities
 			var options = new AppiumOptions { PlatformName = "Android" };
 			options.AddAdditionalCapability("deviceName", deviceName);
@@ -53,23 +57,23 @@ namespace UnitTestProject1
 			return driver;
 		}
 
-		public void DestroyAndroidDriver(AndroidDriver<IWebElement> androidDriver)
+		public void DestroyDriver(AndroidDriver<IWebElement> driver)
 		{
-			androidDriver.Quit();
+			driver.Quit();
 			_appiumLocalService.Dispose();
 		}
 
-		public void TestMethod(AndroidDriver<IWebElement> androidDriver)
+		public void TestMethod(AndroidDriver<IWebElement> driver)
 		{
 			var homeButtonId = @"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout[1]";
 			const string roomsButtonId = @"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout[2]";
 			const string roomsTitle = "com.crestron.phoenix.touchscreen:id/fragmentRoomsTitle";
 
-			var wait = new WebDriverWait(androidDriver, TimeSpan.FromSeconds(10));
-			var roomsButton = wait.Until(webDriver => androidDriver.FindElementByXPath(roomsButtonId));
+			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+			var roomsButton = wait.Until(webDriver => driver.FindElementByXPath(roomsButtonId));
 			roomsButton.Click();
 
-			var roomTitle = wait.Until(webDriver => androidDriver.FindElementById(roomsTitle).Text);
+			var roomTitle = wait.Until(webDriver => driver.FindElementById(roomsTitle).Text);
 
 			//var roomTitle = driver.FindElementById("com.crestron.phoenix.touchscreen:id/fragmentRoomsTitle").Text;
 			Assert.AreEqual(roomTitle, "Rooms");
